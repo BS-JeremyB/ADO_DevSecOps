86cb737d296cb9b0090ce439ebee800a351ed606
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-12-20 10:57:17 +0100

    Add project files.

---
+++ DB_Music/Tables/Artist.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Artist]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [Creation_Date] DATE NOT NULL,
+    [Retirement_Date] DATE NULL,
+    [Country] NVARCHAR(56) NOT NULL,
+
+    CONSTRAINT CK_RETIREMENT_DATE CHECK  ([Retirement_Date] > [Creation_Date])
+)
---
+++ DB_Music/Tables/Music.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Music]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY,
+	[Title] NVARCHAR(250) NOT NULL,
+	[Release_Date] Date NULL,
+	[Duration] TIME NULL,
+	[Spotify_Link] NVARCHAR(MAX) NULL,
+
+	CONSTRAINT UK_Title_Release_Date UNIQUE ([Title], [Release_Date])
+
+
+)
