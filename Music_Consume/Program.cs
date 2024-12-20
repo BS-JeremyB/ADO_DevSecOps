29ec0e49cf5b958f8c2741da2eecf92c351422d6
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-12-20 17:01:06 +0100

    Table Intermediaire & jointure

--- Music_Consume/Program.cs
+++ Music_Consume/Program.cs
@@ -69,26 +69,86 @@ string connectionString = "Data Source=(localdb)\\BStormLocalDB;Initial Catalog=
 //}
 
 
-using(SqlConnection connection = new SqlConnection(connectionString))
+//using(SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    using(SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "SELECT * FROM dbo.[Artist] WHERE name like 'aya%'";
+
+//        connection.Open();
+
+//        using (SqlDataReader reader = command.ExecuteReader()) { 
+//            while (reader.Read())
+//            {
+//                int id = (int)reader["Id"];
+//                string name = reader["Name"].ToString();
+//                DateTime creationDate = (DateTime)reader["Creation_Date"];
+//                DateTime retirementDate = (DateTime)reader["Retirement_Date"];
+//                string country = reader["Country"].ToString();
+
+//                Console.WriteLine($"{id} - {name} qui débute sa carrière le {creationDate.ToString("dd-MM-yyyy")} vient de {country} ");
+//            }
+
+//        }
+//    }
+//}
+
+
+
+// Exo 2 Ajout Music et lien dans la table intermédiaire
+
+
+//using (SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    using(SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "INSERT INTO dbo.[Music] OUTPUT Inserted.Id VALUES ('Djadja', '2018-04-06', '2:55', 'spotify.com/music/playlist/ayanakamura.com')";
+
+//        connection.Open();
+
+//        int id = (int)command.ExecuteScalar();
+
+//        Console.WriteLine(id);
+
+//    }
+//};
+
+int idArtistMusic;
+using (SqlConnection connection = new SqlConnection(connectionString))
 {
-    using(SqlCommand command = connection.CreateCommand())
+    using (SqlCommand command = connection.CreateCommand())
     {
-        command.CommandText = "SELECT * FROM dbo.[Artist] WHERE name like 'aya%'";
+        command.CommandText = "INSERT INTO dbo.[Artist_Music] OUTPUT Inserted.Id VALUES (1,2)";
 
         connection.Open();
 
-        using (SqlDataReader reader = command.ExecuteReader()) {
+        idArtistMusic = (int)command.ExecuteScalar();
+
+        Console.WriteLine(idArtistMusic);
+
+    }
+};
+
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    using (SqlCommand command = connection.CreateCommand())
+    {
+        command.CommandText = "SELECT A.Name, M.Title " +
+            "FROM Artist_Music AM " +
+            "JOIN Artist A ON AM.Artist_Id = A.Id " +
+            "JOIN Music M ON AM.Music_Id = M.Id " +
+            $"";
+
+        connection.Open();
+
+        using (SqlDataReader reader = command.ExecuteReader()) 
+        {
             while (reader.Read())
             {
-                int id = (int)reader["Id"];
-                string name = reader["Name"].ToString();
-                DateTime creationDate = (DateTime)reader["Creation_Date"];
-                DateTime retirementDate = (DateTime)reader["Retirement_Date"];
-                string country = reader["Country"].ToString();
-
-                Console.WriteLine($"{id} - {name} qui débute sa carrière le {creationDate.ToString("dd-MM-yyyy")} vient de {country} ");
+                Console.WriteLine($"{reader["Name"].ToString()} - {reader["Title"].ToString()}");
             }
-
         }
+
     }
 }
