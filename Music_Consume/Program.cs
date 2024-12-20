2edaaa1049404147822e268c0ed1fc8a7285d1be
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-12-20 15:15:06 +0100

    non query / Reader

--- Music_Consume/Program.cs
+++ Music_Consume/Program.cs
@@ -13,15 +13,82 @@ string connectionString = "Data Source=(localdb)\\BStormLocalDB;Initial Catalog=
 
 // Demander la date à la DB !
 
+//using(SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    using (SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "SELECT GETDATE() as date";
+
+//        connection.Open();
+//        string date = command.ExecuteScalar().ToString();
+
+//        Console.WriteLine(date);
+//    }
+//}
+
+// Exercice 1, ajouter une artiste
+// Aya Nakamura
+// 2014
+// 2014
+// France
+
+//using (SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    using (SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "INSERT INTO dbo.[Artist] " +
+//            "OUTPUT inserted.Id " +
+//            "VALUES('Aya Nakamura', '2014-01-01', '2014-01-02', 'FRANCE')";
+
+
+//        connection.Open();
+//        int id = (int)command.ExecuteScalar();
+//        Console.WriteLine(id);
+//    }
+//}
+
+
+//using (SqlConnection connection = new SqlConnection(connectionString))
+//{
+//    using(SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "DELETE FROM dbo.[Artist] WHERE id = 1";
+
+//        connection.Open();
+//        int rows = command.ExecuteNonQuery();
+
+//        if(rows > 0)
+//        {
+//            Console.WriteLine("bye aya");
+//        }
+//        else
+//        {
+//            Console.WriteLine("toujours là");
+//        }
+//    }
+//}
+
+
 using(SqlConnection connection = new SqlConnection(connectionString))
 {
-    using (SqlCommand command = connection.CreateCommand())
+    using(SqlCommand command = connection.CreateCommand())
     {
-        command.CommandText = "SELECT GETDATE() as date";
+        command.CommandText = "SELECT * FROM dbo.[Artist] WHERE name like 'aya%'";
 
         connection.Open();
-        string date = command.ExecuteScalar().ToString();
 
-        Console.WriteLine(date);
+        using (SqlDataReader reader = command.ExecuteReader()) {
+            while (reader.Read())
+            {
+                int id = (int)reader["Id"];
+                string name = reader["Name"].ToString();
+                DateTime creationDate = (DateTime)reader["Creation_Date"];
+                DateTime retirementDate = (DateTime)reader["Retirement_Date"];
+                string country = reader["Country"].ToString();
+
+                Console.WriteLine($"{id} - {name} qui débute sa carrière le {creationDate.ToString("dd-MM-yyyy")} vient de {country} ");
+            }
+
+        }
     }
 }
